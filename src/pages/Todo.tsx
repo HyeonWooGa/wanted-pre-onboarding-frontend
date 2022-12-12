5d0590ff02fadbd0e9b9a3c1a849f985581ec6a8
HyeonWooGa <yeonwoopark22@gmail.com>
2022-12-13 00:04:05 +0900

    ✨ feat: 할 일 페이지 렌더링 및 기능 작업

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,8 +1,38 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GNB } from "../components/GNB";
+import { ListTodos } from "../components/ListTodos";
+import { WriteTodo } from "../components/WriteTodo";
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+export interface ITodo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
 
 export const Todo = () => {
+  const [todos, setTodos] = useState<ITodo[] | null>(null);
+  const [doRendering, setDoRendering] = useState(false);
+  const getTodos = async () => {
+    try {
+      const res = await axios.get(baseUrl + "/todos", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+      setTodos(res.data);
+    } catch (error) {
+      alert("할 일 목록을 불러오는 데 실패했습니다");
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    localStorage.getItem("access_token") && getTodos();
+  }, [doRendering]);
   const navigate = useNavigate();
   useEffect(() => {
     !localStorage.getItem("access_token") && navigate("/");
@@ -12,6 +42,16 @@ export const Todo = () => {
       <header>
         <GNB hasSigned={true} setHasSigned={() => {}} />
       </header>
+      <main className="w-screen h-screen flex flex-col items-center mt-20 gap-8">
+        <section className="flex flex-col gap-1">
+          <label>할일 생성</label>
+          <WriteTodo setDoRendering={setDoRendering} />
+        </section>
+        <section className="flex flex-col gap-1">
+          <label>할일 목록</label>
+          <ListTodos todos={todos} setDoRendering={setDoRendering} />
+        </section>
+      </main>
     </>
   );
 };
